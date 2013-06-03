b2eaab6de5d95ac488187351f006e45589c33e52
Niklas Lundberg <niklas.lundberg@jayway.com>
2013-06-03 15:59:25 +0200

    Added version number output.

--- source/Arbor.Castanea/CastaneaApplication.cs
+++ source/Arbor.Castanea/CastaneaApplication.cs
@@ -1,16 +1,24 @@
-﻿namespace Arbor.Castanea
+﻿using System;
+using System.Reflection;
+
+namespace Arbor.Castanea
 {
     public class CastaneaApplication
     {
         public int RestoreAllSolutionPackages(NuGetConfig nuGetConfig)
         {
+            Assembly entryAssembly = Assembly.GetExecutingAssembly();
+            Version version = entryAssembly.GetName().Version;
+            
+            CastaneaLogger.Write(GetType().Namespace + ", " + version);
+
             var helper = new NuGetHelper();
 
-            var ensuredConfig = helper.EnsureConfig(nuGetConfig);
+            var repositoriesConfig = helper.EnsureConfig(nuGetConfig);
 
-            var repositories = helper.GetNuGetRepositories(ensuredConfig);
+            var repositories = helper.GetNuGetRepositories(repositoriesConfig);
 
-            return helper.RestorePackages(repositories, ensuredConfig);
+            return helper.RestorePackages(repositories, repositoriesConfig);
         }
     }
 }
